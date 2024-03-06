87fb018317a9b61a9b61a62deee6864146f8c287
oshogwe ikekhai <123867684+oshogwe@users.noreply.github.com>
2024-03-06 18:30:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          understand React
+          understanding React
         </a>
       </header>
     </div>
