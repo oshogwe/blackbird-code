fffe98a453e154b4ae7eef8ae1dc4bae3ff937f6
oshogwe ikekhai <osho/ikekhai@yahoo.com>
2024-03-06 01:50:23 +0100

    committing blackbird code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactjs.orghttps://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
